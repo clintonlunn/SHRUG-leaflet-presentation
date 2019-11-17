c2eda2c2bf020cb0fbd2632fbf2f9ec5152a50a8
Clinton Lunn <clintonlunn@Clintons-MacBook-Pro.local>
2019-11-17 14:57:23 -0500

    add esri leaflet example

--- js/site.js
+++ js/site.js
@@ -4,17 +4,6 @@
 	  		zoomControl: false
 	  	}).setView([30.4383, -84.2807], 15);
 
-
-	  	// var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-	  	// 	'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-	  	// 	'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-	  	// 	mbUrl = 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png';
-
-	  	// L.tileLayer(mbUrl, {
-	  	// 	id: 'uafrazier.o3ecma8l',
-	  	// 	attribution: mbAttr
-	  	// }).addTo(header_map);
-
 	  	L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	  		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	  		subdomains: 'abcd',
